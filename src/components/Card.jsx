1ff2e11271ee92b6bc37a165c2dc89873e6d079e
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-10-27 13:59:52 +0100

    Made card flipped

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
-const Card = ({elem}) => {
+import { useState } from "react";
 
+
+const Card = ({elem}) => {
+    const [isFlipped, setFlipped] = useState(false);
+
+    const onFlipCard = () => {
+        setFlipped(true)
+    }
 
     return (
-        <div className="w-full flex justify-center items-center aspect-square rounded-2xl border border-black">
-            <div id="front" className="text-xl">
+        <div onClick={onFlipCard} className="w-full flex justify-center items-center aspect-square rounded-2xl border border-black">
+            <div id="front" className={`text-xl ${isFlipped ? "hidden" : ""}`}>
                 <h3>FRONT</h3>
             </div>
-            <div id="back" className="text-7xl hidden">
+            <div id="back" className={`${isFlipped ? "" : "hidden"}`}>
                 {elem}
             </div>
         </div>
--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import Card from "./Card";
 
 const cardElement = [
@@ -27,13 +26,15 @@ const cardElement = [
 
 
 const CardGrid = () => {
-    // const [isFlipped, setFlipped] = useState(false);
 
     return (
         <section className="grid grid-cols-5 gap-5 p-10">
             { cardElement.map((elem, idx) => {
                 return (
-                    <Card key={idx} elem={elem}/>
+                    <Card 
+                        key={idx} 
+                        elem={elem}
+                        />
                 )
             })}
         </section>
