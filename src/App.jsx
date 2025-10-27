7bec4dd8775eb9eebc184b80557669671ce091fd
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-10-27 12:42:42 +0100

    Create 2 components card and car grid and install tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500 underline'>TEXT REACT</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CardGrid.jsx
@@ -0,0 +1,11 @@
+const CardGrid = () => {
+
+
+    return (
+        <section>
+            
+        </section>
+    )
+}
+
+export default CardGrid;
